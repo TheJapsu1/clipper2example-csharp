fd485e925916886bb25f54bfaaf6ae3bf8afbcfd
japsu <japsu.honkasalo@gmail.com>
2022-11-04 20:34:02 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Clipper2Lib;
+
+Console.WriteLine("Hello, World!");
+
+PathsD polyline = new PathsD();
+
+double[] path = new[]
+{
+    100.100,
+    1500.100,
+    100.1500,
+    1500.1500
+};
+
+polyline.Add(Clipper.MakePath(path));
+
+// Offset polyline
+PathsD solution = Clipper.InflatePaths(polyline, 200, JoinType.Miter, EndType.Square);
+
+// Draw polyline & use it
